0ec4f6e6f176446c710f8dc3908009bcdb7d3276
Wenjing Wang <wenjing.liu.wang@gmail.com>
2017-01-18 23:04:58 -0500

    Add files via upload

---
+++ Lab3/Lab2.java
@@ -0,0 +1,64 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Lab2 {
+	public static void main(String[] args) {
+
+		Numbers num = new Numbers();
+		Scanner sc = new Scanner(System.in);
+
+		boolean done = false;
+		int option = 0;
+
+		try {
+			while (!done) {
+				System.out
+						.println("Enter 1 to create array of new size,\n" + "2 to generate random numbers into array,\n"
+								+ "3 to count a value, 4 to display array,\n" + "5 to insertion sort, 6 to merge sort, 0 to quit:");
+
+				option = sc.nextInt();
+
+				switch (option) {
+				case 1:
+					num.newSize();
+					break;
+
+				case 2:
+					num.generateNum();
+					break;
+
+				case 3:
+					num.count();
+					break;
+
+				case 4:
+					num.display();
+					break;
+
+				case 5:
+					num.insertionSort();
+					break;
+					
+				case 6:
+					num.mergeSort();
+					break;
+
+				case 0:
+					System.out.println("Quit");
+					done = true;
+					break;
+
+				default:
+					System.out.println("Invalid option, please try again");
+					break;
+				}
+				System.out.println();
+			}
+
+		} catch (InputMismatchException e) {
+			System.out.println("ERROR!!");
+		}
+
+	}
+
+}
--- Lab3/Numbers.java
+++ Lab3/Numbers.java
@@ -1,1 +1,131 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Numbers {
+	private int[] numbers = null;
+	private Scanner sc = new Scanner(System.in);
+	private int[] helper;
+
+	public Numbers() {
+		this.numbers = new int[100]; // important!!! To initialize the array'
+										// size.
+	}
+
+	public void newSize() {
+		int size = 0;
+		System.out.println("Enter the size of numbers");
+		size = sc.nextInt();
+
+		numbers = Arrays.copyOf(numbers, size);
+
+		System.out.println("Array has been generated");
+	}
+
+	public void generateNum() {
+		Random random = new Random();
+
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = random.nextInt(100);
+
+		}
+
+		System.out.println("Array has been generated successfully");
+
+	}
+
+	public void count() {
+		if (numbers.length == 0) {
+			System.out.println("There are nothing to count");
+		} else {
+			int count = 0;
+			int number = 0;
+
+			System.out.println("Enter the number you want to search");
+			number = sc.nextInt();
+
+			for (int i = 0; i < numbers.length; i++) {
+				if (numbers[i] == number) {
+					count++;
+				}
+			}
+
+			System.out.println("There are " + count + " of the number " + number);
+		}
+
+	}
+
+	public void insertionSort() {
+		int temp = 0;
+
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = i; j < numbers.length; j++) {
+				if (numbers[j] < numbers[i]) {
+					temp = numbers[i];
+					numbers[i] = numbers[j];
+					numbers[j] = temp;
+				}
+			}
+		}
+
+		System.out.println("Array has been sorted");
+	}
+
+	public void display() {
+		for (int nums : numbers) {
+			System.out.println(nums);
+		}
+	}
+
+	public void mergeSort() {
+		mergesort(numbers);
+	}
+
+	private static void mergesort(int[] numbers) {
+		if (numbers.length == 1)
+			return;
+
+		int middle = numbers.length / 2;
+		int[] a1 = new int[middle];
+		int[] a2 = new int[numbers.length - middle];
+
+		for (int i = 0; i < numbers.length; i++) {
+			if (i < middle)
+				a1[i] = numbers[i];
+			else
+				a2[i - middle] = numbers[i];
+		}
+
+		// sort two parts
+		mergesort(a1);
+		mergesort(a2);
+
+		// merge two parts together
+		merge(a1, a2, numbers);
+	}
+
+	/**
+	 * To merge two parts sorted array together and sort this two parts.
+	 */
+	private static void merge(int[] a1, int[] a2, int[] values) {
+		int index1 = 0;
+		int index2 = 0;
+
+		for (int j = 0; j < values.length; j++) {
+			if (index1 == a1.length) {
+				values[j] = a2[index2];
+				index2++;
+			} else if (index2 == a2.length) {
+				values[j] = a1[index1];
+				index1++;
+			} else if (a1[index1] < a2[index2]) {
+				values[j] = a1[index1];
+				index1++;
+			} else {
+				values[j] = a2[index2];
+				index2++;
+			}
+		}
+
+	}
+}
